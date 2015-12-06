68a2878e4f3e198947c740ec9dad14dfb26abc81
Joe Neuman <Joe Neuman>
2015-12-05 21:37:53 -0700

    first 3 of practice done and added index

--- practice.js
+++ practice.js
@@ -2,8 +2,14 @@
 
 
 //Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
+function me() {
+  var me = {
+    name: "Joe",
+    age: 49
+  };
+  alert("Name: " + me.name + "\nAge: " + me.age);
+}
 
-  //Code here
 
 
 
@@ -15,17 +21,28 @@
 
 //Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
 
-  //Code here
+var favoriteThings = {
+  band: "The Cure",
+  food: "Pizza",
+  person: "Lolo",
+  book: "bom",
+  movie: "Ground Hog Day",
+  holiday: "Christmas"
+};
+
 
 
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
-  //Code here
+favoriteThings.car = "Ford";
+favoriteThings.brand = "Samsung";
+
 
 
 //Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'.
 
-  //Code here
+favoriteThings.food = "Lazanya";
+favoriteThings.book = "BoM";
 
 
 
